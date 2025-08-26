997bf14bb3fd9da12a2b45f66af74132b1115974
Terry Heo <terryheo@google.com>
2025-08-26 14:30:41 -0700

    Clean up Vendors CC API
    - Do not use C API targets directly
    
    LiteRT-LM-PiperOrigin-RevId: 799720289

--- runtime/engine/io_types_test.cc
+++ runtime/engine/io_types_test.cc
@@ -10,10 +10,9 @@
 #include <gtest/gtest.h>
 #include "absl/status/status.h"  // from @com_google_absl
 #include "absl/time/clock.h"  // from @com_google_absl
-#include "runtime/util/test_utils.h"  // NOLINT
 #include "absl/time/time.h"  // from @com_google_absl
 #include "absl/types/span.h"  // from @com_google_absl
-#include "litert/c/litert_model.h"  // from @litert
+#include "litert/c/litert_model_types.h"  // from @litert
 #include "litert/c/litert_tensor_buffer_types.h"  // from @litert
 #include "litert/cc/litert_environment.h"  // from @litert
 #include "litert/cc/litert_layout.h"  // from @litert
@@ -21,6 +20,7 @@
 #include "litert/cc/litert_tensor_buffer.h"  // from @litert
 #include "litert/test/matchers.h"  // from @litert
 #include "runtime/util/convert_tensor_buffer.h"
+#include "runtime/util/test_utils.h"  // NOLINT
 
 namespace litert::lm {
 namespace {
