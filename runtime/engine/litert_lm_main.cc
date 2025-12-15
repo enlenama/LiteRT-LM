28199d36c861232347ca8fea8a3eae945d986cfb
Google AI Edge <ai-edge-bot@google.com>
2025-12-14 20:54:14 -0800

    Automated Code Change
    LiteRT-LM-PiperOrigin-RevId: 844564678

--- runtime/engine/litert_lm_main.cc
+++ runtime/engine/litert_lm_main.cc
@@ -29,7 +29,6 @@
 
 #include "absl/base/log_severity.h"  // from @com_google_absl
 #include "absl/flags/flag.h"  // from @com_google_absl
-#include "absl/flags/parse.h"  // from @com_google_absl
 #include "absl/functional/any_invocable.h"  // from @com_google_absl
 #include "absl/log/absl_check.h"  // from @com_google_absl
 #include "absl/log/absl_log.h"  // from @com_google_absl
@@ -39,7 +38,6 @@
 #include "absl/strings/string_view.h"  // from @com_google_absl
 #include "absl/time/time.h"  // from @com_google_absl
 #include "nlohmann/json.hpp"  // from @nlohmann_json
-#include "litert/c/internal/litert_logging.h"  // from @litert
 #include "runtime/conversation/conversation.h"
 #include "runtime/conversation/io_types.h"
 #include "runtime/engine/engine.h"
@@ -116,10 +114,9 @@ std::string GetInputPrompt() {
 }
 
 absl::Status MainHelper(int argc, char** argv) {
-  absl::ParseCommandLine(argc, argv);
   // Overrides the default for FLAGS_minloglevel to error.
   absl::SetMinLogLevel(absl::LogSeverityAtLeast::kError);
-  LiteRtSetMinLoggerSeverity(LiteRtGetDefaultLogger(), LITERT_SILENT);
+  absl::SetStderrThreshold(absl::LogSeverityAtLeast::kFatal);
 
   const std::string model_path = absl::GetFlag(FLAGS_model_path);
   if (model_path.empty()) {
@@ -143,9 +140,10 @@ absl::Status MainHelper(int argc, char** argv) {
 
   // Create the conversation.
   std::unique_ptr<Conversation> conversation;
-  ASSIGN_OR_RETURN(auto conversation_config,
-                   ConversationConfig::CreateFromSessionConfig(
-                       *engine, litert::lm::SessionConfig::CreateDefault()));
+  auto session_config = litert::lm::SessionConfig::CreateDefault();
+  ASSIGN_OR_RETURN(
+      auto conversation_config,
+      ConversationConfig::CreateFromSessionConfig(*engine, session_config));
   ASSIGN_OR_RETURN(conversation,
                    Conversation::Create(*engine, conversation_config));
 
@@ -154,10 +152,6 @@ absl::Status MainHelper(int argc, char** argv) {
   const std::string input_prompt = GetInputPrompt();
   std::cout << "input_prompt: " << input_prompt << std::endl;
   content_list.push_back({{"type", "text"}, {"text", input_prompt}});
-  RETURN_IF_ERROR(conversation->SendMessageAsync(
-      json::object({{"role", "user"}, {"content", content_list}}),
-      CreateMessageCallback()));
-  RETURN_IF_ERROR(engine->WaitUntilDone(absl::Minutes(10)));
 
   // Print the benchmark info.
   auto benchmark_info = conversation->GetBenchmarkInfo();
